866d0b51d5dce4fdfb9b3a9a784734ea4ebc39b0
MattiasAndersen <mattias.andersen@blinkabla.se>
2022-05-19 09:07:35 +0200

    add bio styling

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Bio from '../../components/Bio/Bio';
+import Contact from '../../components/Contact/Contact';
 
 const Home = () => {
   return (
     <>
       <Bio />
+      <Contact />
     </>
   );
 };
