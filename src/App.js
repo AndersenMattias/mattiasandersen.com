07e9c47f09a1065ad1da225a40a4ced06a71dde9
MattiasAndersen <mattias.andersen@blinkabla.se>
2022-05-19 09:01:11 +0200

    start work on styling

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import { Route, Routes } from 'react-router-dom';
+import Header from './components/Header/Header';
+import ProjectDetails from './components/ProjectDetails/ProjectDetails';
+
+import Home from './Pages/Home/Home';
+import Projects from './Pages/Projects/Projects';
+
+import './styles/base.scss';
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Routes>
+        <Route path='/' exact element={<Home />} />
+        <Route path='/projects' exact element={<Projects />} />
+        <Route path='/projects/:id' exact element={<ProjectDetails />} />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
