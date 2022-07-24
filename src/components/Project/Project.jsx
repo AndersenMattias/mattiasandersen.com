646c8a5ce4b9a6c50195198454b643d2c3bac8a0
MattiasAndersen <mattias.andersen@blinkabla.se>
2022-07-24 16:44:41 +0200

    test

--- src/components/Project/Project.jsx
+++ src/components/Project/Project.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-import { projectsData } from '../../content/projects';
 import { Icon, IconContainer } from '../Icon/Icon';
 
 import '../../styles/components/_projectList.scss';
--- src/components/ProjectDetails/ProjectDetails.jsx
+++ src/components/ProjectDetails/ProjectDetails.jsx
@@ -1,66 +1,15 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
 
-import { projectsData } from '../../content/projects';
-import { Icon, IconContainer } from '../Icon/Icon';
-
-import { FaGithub } from 'react-icons/fa';
-
 const ProjectDetails = () => {
   const { id } = useParams();
 
   // eslint-disable-next-line
-  let project = projectsData.find((x) => x.id == id);
+  // let project = projectsData.find((x) => x.id == id);
 
   return (
     <div className='projects-wrapper'>
-      <div key={project.id} className='projectList-container'>
-        <div className='projectList-inner'>
-          <div className='project-imageContainer'>
-            <img className='project-image' src={project.image} alt='project' />
-          </div>
-          <div>
-            <h4 className='project-title'>{project.title}</h4>
-          </div>
-        </div>
-
-        <div className='projectList-intro'>
-          <div className='intro-txt'>
-            <p>{project.intro}</p>
-          </div>
-          <div className='tech-container'>
-            {project.tech.map((tech, index) => (
-              <IconContainer key={index}>
-                <Icon key={tech.key} title={tech.value} />
-              </IconContainer>
-            ))}
-          </div>
-        </div>
-
-        <div className='project__links'>
-          <a
-            className='project__link'
-            href={project.githubUrl}
-            target='_blank'
-            rel='noopener noreferrer'
-          >
-            <FaGithub /> <p>View source</p>
-          </a>
-          <a
-            className='project__link'
-            href={project.hostUrl}
-            target='_blank'
-            rel='noopener noreferrer'
-          >
-            <div className='url-container'>
-              <p className='url-txt'>Live demo:</p>
-              <div>
-                <p>{project.hostUrl ? project.hostUrl : 'Ingen Url'}</p>
-              </div>
-            </div>
-          </a>
-        </div>
-      </div>
+      <div>project details</div>
     </div>
   );
 };
