f22369062363dd6228d8c8dab2959c4f18006a9d
MattiasAndersen <mattias.andersen@blinkabla.se>
2022-07-24 16:46:53 +0200

    wip

--- src/components/ProjectDetails/ProjectDetails.jsx
+++ src/components/ProjectDetails/ProjectDetails.jsx
@@ -1,12 +1,6 @@
 import React from 'react';
-import { useParams } from 'react-router-dom';
 
 const ProjectDetails = () => {
-  const { id } = useParams();
-
-  // eslint-disable-next-line
-  // let project = projectsData.find((x) => x.id == id);
-
   return (
     <div className='projects-wrapper'>
       <div>project details</div>
