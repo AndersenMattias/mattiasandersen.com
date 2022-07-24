4cd973228739e901c48be9c513293fa0483315f7
MattiasAndersen <mattias.andersen@blinkabla.se>
2022-07-24 11:07:52 +0200

    setup apollo with test query

--- src/Pages/Projects/Projects.jsx
+++ src/Pages/Projects/Projects.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
+import { useQuery } from '@apollo/client';
 import Project from '../../components/Project/Project';
+import { GET_PROJECTS } from '../../queries/project';
 
 const Projects = () => {
+  const { loading, error, data } = useQuery(GET_PROJECTS);
+  console.log(data);
+
+  if (loading) return <p>Loading..</p>;
+  if (error) return <p>Something went wrong..</p>;
+
   return (
     <div>
-      <Project />
+      {data.projects.map((project) => (
+        <Project key={project.id} project={project} />
+      ))}
     </div>
   );
 };
